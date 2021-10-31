9c0b3d96f71f6d3b163b9eb11addb2f1752719b1
Mohammad Ishtiaq <ishtiaqm31@gmail.com>
2021-10-31 00:50:59 -0400

    Gameplay

---
+++ Programming-Language/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    public int speed;
+
+    public bool goForward = true;
+    public bool goRight = false;
+    public bool goLeft = false;
+
+    public bool rotateRight = false;
+    public bool rotateleft = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (goForward == true)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        else if(goRight == true)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        else if (goLeft == true)
+        {
+            transform.position += transform.right * -speed * Time.deltaTime;
+        }
+
+
+        if (rotateRight == true)
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * speed);
+        }
+        else if (rotateleft == true)
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * -speed);
+        }
+    }
+}
---
+++ Programming-Language/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed = 0f;
+
+    private int desiredLane = 1;//0:left, 1:middle, 2:right
+    public float laneDistance = 4.25f;//The distance between two lanes
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Change the position pased on the z axis
+        direction.z = forwardSpeed;
+
+        //Keyboard Inputs
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+            {
+                desiredLane = 2;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+        }
+
+        //Calculae where we should be in the future
+        //Calculate where we should be in the future
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (desiredLane == 0)
+            targetPosition += Vector3.left * laneDistance;
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * laneDistance;
+
+        //transform.position = targetPosition;
+        if (transform.position != targetPosition)
+        {
+            Vector3 diff = targetPosition - transform.position;
+            Vector3 moveDir = diff.normalized * 30 * Time.deltaTime;
+            if (moveDir.sqrMagnitude < diff.magnitude)
+                controller.Move(moveDir);
+            else
+                controller.Move(diff);
+        }
+
+        controller.Move(direction * Time.deltaTime);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
