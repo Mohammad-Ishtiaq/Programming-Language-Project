aae569cd1eb22215ca80524f42f45c333d90b03b
Mohammad Ishtiaq <ishtiaqm31@gmail.com>
2021-11-01 00:31:20 -0400

    updates

---
+++ Programming-Language/Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<AudioManager>().PlaySound("CoinPickup");
+
+            PlayerManager.numberOfCoins += 1;
+            
+            Destroy(gameObject);
+        }
+    }
+}
--- Programming-Language/Assets/Scripts/PlayerController.cs
+++ Programming-Language/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed = 0f;
+    public float maxSpeed;
 
     private int desiredLane = 1;//0:left, 1:middle, 2:right
     public float laneDistance = 4.25f;//The distance between two lanes
@@ -24,8 +25,13 @@ public class PlayerController : MonoBehaviour
         //Change the position pased on the z axis
         direction.z = forwardSpeed;
 
+        if (forwardSpeed <= maxSpeed)
+        {
+            forwardSpeed += 0.1f * Time.deltaTime;
+        }
+
         //Keyboard Inputs
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             desiredLane++;
             if (desiredLane == 3)
@@ -42,6 +48,26 @@ public class PlayerController : MonoBehaviour
                 desiredLane = 0;
             }
         }
+        
+        //Swipe Inputs
+        if (SwipeManager.swipeRight)
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+            {
+                desiredLane = 2;
+            }
+        }
+
+        if (SwipeManager.swipeLeft)
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+        }
+
 
         //Calculae where we should be in the future
         //Calculate where we should be in the future
@@ -55,20 +81,29 @@ public class PlayerController : MonoBehaviour
         if (transform.position != targetPosition)
         {
             Vector3 diff = targetPosition - transform.position;
-            Vector3 moveDir = diff.normalized * 30 * Time.deltaTime;
+            Vector3 moveDir = diff.normalized * 80 * Time.deltaTime;
             if (moveDir.sqrMagnitude < diff.magnitude)
                 controller.Move(moveDir);
             else
                 controller.Move(diff);
         }
 
-        controller.Move(direction * Time.deltaTime);
+        //controller.Move(direction * Time.deltaTime);
 
 
     }
 
     private void FixedUpdate()
     {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "Obstacle")
+        {
+            FindObjectOfType<AudioManager>().PlaySound("CarCrash");
+            PlayerManager.gameOver = true;
+        }
     }
 }
